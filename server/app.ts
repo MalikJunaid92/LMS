ae139c2aabbcef97b276fb1f15cf04aed3d6754e
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-08-05 01:07:00 +0500

    setup backend

---
+++ server/app.ts
@@ -0,0 +1,30 @@
+import express, { NextFunction, Request, Response } from "express";
+require("dotenv").config();
+export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+// body parser
+app.use(express.json({ limit: "50mb" }));
+
+// cookie-parser
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+// testing
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
+});
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
