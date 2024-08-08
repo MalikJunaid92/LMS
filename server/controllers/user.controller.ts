7cb2b4e195373357534c5eb671120a1375aca0d7
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-08-08 17:30:23 +0500

    create activate user api

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 require("dotenv").config();
 import { Request, Response, NextFunction } from "express";
-import userModel from "../models/user.model";
+import userModel, { IUser } from "../models/user.model";
 import ErrorHandler from "../utilis/ErrorHandler";
 import { catchAsyncError } from "../middleware/catchAsyncError";
 import jwt, { Secret } from "jsonwebtoken";
@@ -69,3 +69,38 @@ export const createActivationToken = (user: any): IActivationToken => {
   );
   return { token, activationCode };
 };
+// activate user
+interface IActivationRequest {
+  activation_token: string;
+  activation_code: string;
+}
+export const activateUser = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { activation_token, activation_code } =
+        req.body as IActivationRequest;
+      const newUser: { user: IUser; activationCode: string } = jwt.verify(
+        activation_token,
+        process.env.ACTIVATION_TOKEN_SECRET as string
+      ) as { user: IUser; activationCode: string };
+      if (newUser.activationCode !== activation_code) {
+        return next(new ErrorHandler("Email already exist", 400));
+      }
+      const { email, name, password } = newUser.user;
+      const emailExist = await userModel.findOne({ email });
+      if (emailExist) {
+        return next(new ErrorHandler("Email already exist", 400));
+      }
+      const user = await userModel.create({
+        name,
+        email,
+        password,
+      });
+      res.status(201).json({
+        success: true,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
