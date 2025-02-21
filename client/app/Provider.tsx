8c6a0600fc7236731c5f6c24e2d51279ccf87ed7
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2025-02-21 23:22:04 +0500

    make useRegisterMutation, useActivationMutation

--- client/app/Provider.tsx
+++ client/app/Provider.tsx
@@ -6,6 +6,6 @@ interface Props {
   children: ReactNode;
 }
 
-export function AppProvider({ children }: Props) {
+export function Providers({ children }: Props) {
   return <Provider store={store}>{children}</Provider>;
 }
---
+++ client/redux/features/auth/authApi.ts
@@ -0,0 +1,49 @@
+import { apiSlice } from "../api/apiSlice";
+import { userRegisteration } from "./authSlice"; // Ensure correct import
+
+// Define response type
+type RegistrationResponse = {
+  message: string;
+  activationToken: string;
+};
+
+// Define request data type
+type RegistrationData = {};
+
+export const authApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    // Endpoint for user registration
+    register: builder.mutation<RegistrationResponse, RegistrationData>({
+      query: (data) => ({
+        url: "auth/register",
+        method: "POST",
+        body: data,
+        credentials: "include" as const,
+      }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            userRegisteration({
+              token: result.data.activationToken, // Ensure activationToken exists
+            })
+          );
+        } catch (error) {
+          console.error("Registration Error:", error);
+        }
+      },
+    }),
+    activation: builder.mutation({
+      query: ({ activation_token, activation_code }) => ({
+        url: "activate-user",
+        method: "POST",
+        body: {
+          activation_token,
+          activation_code,
+        },
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterMutation, useActivationMutation } = authApi;
