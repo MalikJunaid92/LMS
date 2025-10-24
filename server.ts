df0ef7ebef3b1ef6248ed8617609e2b238efb499
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2025-10-24 16:54:37 +0500

    updated

---
+++ server.ts
@@ -0,0 +1,19 @@
+import { app } from "./server/api/index";
+import { initSocketServer } from "./server/socketServer";
+import { connectDB } from "./server/utilis/db";
+import cloudinary from "cloudinary";
+import http from "http";
+require("dotenv").config();
+// cloudinary config
+const server = http.createServer(app);
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+initSocketServer(server);
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+  connectDB();
+});
